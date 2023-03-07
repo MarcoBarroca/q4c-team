d1dd02800fa79ea62cf8998e5ecb8c3261ed7f66
Marco Antonio Barroca <magbarroca@gmail.com>
2023-03-07 11:47:49 -0300

    commit

---
+++ Mg+H2O/real_hw_nairobi/run_scripts.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+python real_hw_3.py &
+python real_hw_6.py &
+python real_hw_9.py &
+python real_hw_12.py &
+python real_hw_15.py &
+python real_hw_18.py &
+python real_hw_21.py & 
+python real_hw_24.py &
+python real_hw_27.py &
+python real_hw_30.py &
+python real_hw_33.py
